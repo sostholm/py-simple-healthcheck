4939ca6b3e17b41ab80682f55c1e78f12272a447
root <sostholm@gmail.com>
2022-09-16 09:34:31 +0000

    changing exit exception to exit with code 1 to match docker requirement

--- py_simple_healthcheck/healthcheck.py
+++ py_simple_healthcheck/healthcheck.py
@@ -1,13 +1,10 @@
 from time import time
-from sys import platform
+from sys import platform, exit
 
 if platform == 'linux' or platform == 'linux2':
     filepath = '/tmp/py_simple_healthcheck'
 else:
     filepath = 'py_simple_healthcheck'
-
-class UnhealthyException(Exception):
-    pass
 
 def health_check(timeout):
     with open(filepath, 'w+') as file:
@@ -19,7 +16,7 @@ def is_healthy():
             content = file.read()
             timestamp, timeout = content.split(',')
             if time() > int(timestamp) + int(timeout):
-                raise UnhealthyException()
+                exit(1)
     
     except FileNotFoundError as e:
         print('Healthcheck file not found', flush=True)
